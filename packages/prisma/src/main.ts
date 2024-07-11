48adab9ee7b020df28db6d5c7623cbd7dedd8381
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-07-12 01:08:47 +0200

    refactor: change exports of prisma project

--- packages/prisma/src/main.ts
+++ packages/prisma/src/main.ts
@@ -1,3 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, ChatStatus, MessageRole } from "@prisma/client";
+import type { Chat, Message, User } from "@prisma/client";
 
-export const prismaClient = new PrismaClient();
+export type { Chat, User, Message };
+export { ChatStatus, MessageRole, PrismaClient };
