c79a10d19ce9caa7a6f35a4a075f86324bc4ff14
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-07-13 01:53:23 +0200

    feat: add bcrypt to hash password

--- apps/bff/src/modules/auth/mutations/createUser.ts
+++ apps/bff/src/modules/auth/mutations/createUser.ts
@@ -1,41 +1,65 @@
-import type { MutationResolvers } from "../../../@types/mercurius-generated.js";
+import type { InputMaybe, MutationResolvers } from "../../../@types/mercurius-generated.js";
+import { ErrorType } from "../../../@types/mercurius-generated.js";
+import bcrypt from "bcrypt";
 
 export const createUser: MutationResolvers["createUser"] = async (_parent, args, contextValue) => {
   const { displayName, email, firebaseUid, password } = args.input;
 
   if (firebaseUid && password) {
+    contextValue.app.log.error(contextValue, "cannot create account with password and firebaseUid");
+
     return {
       success: false,
-      error: "cannot create account with password and firebaseUid",
+      error: ErrorType.INTERNAL_ERROR,
     };
   }
 
   if (!firebaseUid && !password) {
+    contextValue.app.log.error(contextValue, "cannot create acount with neither password nor firebase uid");
+
     return {
       success: false,
-      error: "cannot create acount with neither password nor firebase uid",
+      error: ErrorType.INTERNAL_ERROR,
     };
   }
 
   if (!displayName) {
+    contextValue.app.log.error(contextValue, "displayName cannot be undefined nor null");
+
     return {
       success: false,
-      error: "displayName cannot be undefined nor null",
+      error: ErrorType.INTERNAL_ERROR,
     };
   }
 
   if (!email) {
+    contextValue.app.log.error(contextValue, "email cannot be undefined nor null");
+
     return {
       success: false,
-      error: "email cannot be undefined nor null",
+      error: ErrorType.INTERNAL_ERROR,
     };
   }
 
-  contextValue.app.repositories.authRepository.createUser({ displayName, email, firebaseUid, password });
+  const hashedPassword = getHashedPassword(password);
+
+  contextValue.app.repositories.authRepository.createUser({
+    displayName,
+    email,
+    firebaseUid,
+    password: hashedPassword,
+  });
 
   return {
     data: { name: "test1", id: "id_test" },
     success: true,
-    error: "no error",
   };
 };
+
+function getHashedPassword(password: InputMaybe<string> | undefined): string | undefined {
+  if (!password) {
+    return undefined;
+  }
+
+  return bcrypt.hashSync(password, 2);
+}
