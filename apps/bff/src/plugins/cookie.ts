6eb016897ba6d81b559b6408d2b0a2bafb4a1d17
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-07-13 15:08:33 +0200

    fix: cookie plugin initialization

--- apps/bff/src/plugins/cookie.ts
+++ apps/bff/src/plugins/cookie.ts
@@ -1,9 +1,9 @@
 import fp from "fastify-plugin";
-import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import type { FastifyInstance } from "fastify";
 
 export default fp(async (fastify: FastifyInstance) => {
-  fastify.register(fastifyJwt, {
+  fastify.register(fastifyCookie, {
     secret: process.env.COOKIE_SECRET_KEY,
   });
 });
