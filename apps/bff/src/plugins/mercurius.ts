c326d4823868383fee0fe2b629acc4185f0a1913
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-08-09 22:15:18 +0200

    fix: load resolver manually due to incompatibility with vitest

--- apps/bff/src/plugins/mercurius.ts
+++ apps/bff/src/plugins/mercurius.ts
@@ -6,16 +6,20 @@ import path from "path";
 import { fileURLToPath } from "url";
 import mercurius from "mercurius";
 import { loadFilesSync } from "@graphql-tools/load-files";
+import userResolver from "./../modules/auth/resolvers/resolvers.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const resolverArray = loadFilesSync(path.join(__dirname, "./../modules/**/resolvers.ts"));
+// cannot be used with vitest
+// const resolverArray = loadFilesSync(path.join(__dirname, "./../modules/**/resolvers.ts"));
 const typeDefArray = loadFilesSync(path.join(__dirname, "./../modules/**/schema.graphql"));
 
 const schema = makeExecutableSchema({
   typeDefs: mergeTypeDefs(typeDefArray),
-  resolvers: mergeResolvers(resolverArray),
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  resolvers: mergeResolvers([userResolver] as any),
+  // 'as any' is a workaround because of missmatch in types IResolvers of graphql-tools and mercurius
 });
 
 export default fp(async (fastify: FastifyInstance) => {
